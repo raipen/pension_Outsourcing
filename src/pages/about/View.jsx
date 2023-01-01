0362601bc438bce2257a93941ad4ed3317ae7999
Jiwon <raipen@knu.ac.kr>
2023-01-01 17:40:35 +0900

    add Title component

--- src/pages/about/View.jsx
+++ src/pages/about/View.jsx
@@ -3,11 +3,12 @@ import view1 from '../../images/view1.jpg';
 import view2 from '../../images/view2.jpg';
 import view3 from '../../images/view3.jpg';
 import AutoImgSlider from '../../components/AutoImgSlider';
+import Title from '../../components/Title';
 
 const View =  ()=> {
     return (
-      <div style={{marginTop:`70px`}}>
-        <div>VIEW</div>
+      <div>
+        <Title title="VIEW"/>
         <div>| 외부전경</div>
         <AutoImgSlider imgList={[view1,view2,view3]} isRelative={true}/>
       </div>
