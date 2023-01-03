01d6dec80854d99cb4ec719af653056915d4beb6
Jiwon <raipen@knu.ac.kr>
2023-01-03 23:38:29 +0900

    real real final commit

--- src/pages/Main/Page1.jsx
+++ src/pages/Main/Page1.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Page1.module.css';
 import BlurCircle from '../../components/BlurCircle';
 import styled from 'styled-components';
+import LinkButton from '../../components/LinkButton';
 
 const Info = () => {
   return (
@@ -18,6 +19,8 @@ const Info = () => {
         도보 5분이내에 동궁원, 버드파크, 보문호수를 즐기실 수 있고 펜션 입구길에<br />
         보문 맷돌순두부, 함양집, 낙지마실등 맛집들이 즐비하여있습니다.
       </div>
+      <div style={{height: "3rem"}} />
+      <LinkButton href="https://naver.me/GvXbLTLU" text="예약하기" />
     </div>
   );
 }
